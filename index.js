e24e68be0c144986dbaebfec491d896f220be056
sirisharani76 <sirisharani76@gmail.com>
2022-06-16 14:37:02 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+function clearScreen() {
+    document.getElementById("result").value = "";
+}
+
+function liveScreen(value) {
+    document.getElementById("result").value += value; 
+}
+
+function switchTheme() {
+    let darkmode = document.getElementById("dark-mode");
+    let theme = document.getElementById("theme");
+
+    if (theme.getAttribute("href") == "light.css") {
+        theme.href = "dark.css";
+        darkmode.innerHTML = "Light Mode"
+        //darkmode.style.background="white"
+        //darkmode.style.color="black"
+    } else {
+        theme.href = "light.css";
+        darkmode.innerHTML = "Dark Mode"
+    }
+}
